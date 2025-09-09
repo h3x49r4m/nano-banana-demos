fde1f38cd7067c2c0d4f7d828040ddea0aad819f
h3x49r4m <h3x49r4m@outlook.com>
2025-09-09 01:40:42 -0700

    [main] updated run with prompt.txt config. 	modified:   cli/main.py

--- cli/main.py
+++ cli/main.py
@@ -1,14 +1,23 @@
+import argparse
 from gemini_client import GeminiClient
 
+def chat_mode():
+    client = GeminiClient()
+    client.chat()
 
-def test_text_to_image(client):
-    prompt = open('prompts/infographics/dynamic_systems_cartoon_prompt.txt', 'r').readlines() 
+def cmd_mode():
+    parser = argparse.ArgumentParser(description='Generate images from a prompt file.')
+    parser.add_argument('--prompt', type=str, required=True, help='Path to the prompt file.')
+    args = parser.parse_args()
+
+    with open(args.prompt, 'r') as f:
+        prompt = f.readlines()
+
+    client = GeminiClient()
     texts, images = client.respond([prompt])
 
 
-if __name__ == '__main__':
 
-    client = GeminiClient()
-    
-    test_text_to_image(client)
-    #client.chat()
+if __name__ == '__main__':
+    #chat_mode()
+    cmd_mode()
